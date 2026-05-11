c658f027c9ea8185212692dd1b994f64da221521
Alan gael Bernal Miranda <alang131007@gmail.com>
2026-05-11 14:48:25 -0600

    Actualizo notas.sh con rangos completos

--- notas.sh
+++ notas.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-NOTA=6
+NOTA=5
 if [ $NOTA -eq 10 ]; then
-  echo "Aprobaste con excelencia"
-elif [ $NOTA -ge 7 ]; then
-  echo "Aprobaste"
+  echo "Excelente Promedio"
+elif [ $NOTA -ge 6 -a $NOTA -le 7 ]; then
+  echo "aprobaste"
+elif [ $NOTA -ge 8 -a $NOTA -le 9 ]; then
+  echo "felicidades aprobaste"
 else
-  echo "Estas reprobado"
+  echo "reprobaste"
 fi
